425d02a77d9a40af17605253d48e2034634e5b9c
Sandeep Patel <52149614+cs2sandeep@users.noreply.github.com>
2022-11-05 19:18:08 +0530

    Add filter logic

--- scrape.py
+++ scrape.py
@@ -1,9 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-print('a')
 response = requests.get("https://news.ycombinator.com")
-print('b')
 soup = BeautifulSoup(response.text, 'html.parser')
 
 site_links = soup.select('.titleline > a')
@@ -15,10 +13,18 @@ def create_custom_hn(links, votes):
         title = element.getText()
         link = element.get('href', None)
 
-        points = int(site_votes[index].getText().removesuffix(' points'))
+        points = int(votes[index].getText().removesuffix(' points'))
 
         hn.append({'title': title, 'link': link, 'points': points})
     return hn
 
-print(create_custom_hn(site_links, site_votes))
-print(len(create_custom_hn(site_links, site_votes)))
+
+def more_than_100(posts):
+    pop_posts = []
+    generator1 = filter(lambda post: post['points'] > 100, posts)
+    for item in generator1:
+        pop_posts.append(item)
+    return pop_posts
+
+all_posts = create_custom_hn(site_links, site_votes)
+print(more_than_100(all_posts))
