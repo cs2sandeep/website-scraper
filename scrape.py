5e0aaf6ce6bcd5fcffec0b9dbc2da535f9fb1ebd
Sandeep Patel <52149614+cs2sandeep@users.noreply.github.com>
2022-11-06 06:58:30 +0530

    use list comprehension & filter

--- scrape.py
+++ scrape.py
@@ -20,11 +20,7 @@ def create_custom_hn(links, votes):
 
 
 def more_than_100(posts):
-    pop_posts = []
-    generator1 = filter(lambda post: post['points'] > 100, posts)
-    for item in generator1:
-        pop_posts.append(item)
-    return pop_posts
+    return [item for item in filter(lambda post: post['points'] > 100, posts)]
 
 all_posts = create_custom_hn(site_links, site_votes)
 print(more_than_100(all_posts))
